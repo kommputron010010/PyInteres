8b2b1fe5690517811f70ee65ac96f3d87496ac5a
kommputron010010 <86898941+kommputron010010@users.noreply.github.com>
2022-09-19 20:09:01 -0500

    Add files via upload
    primer programa de interes compuesto en python

---
+++ interes.py
@@ -0,0 +1,8 @@
+inicial = 10000
+interes = 0.02
+n = 36
+total = (inicial *((1 + interes) ** n) )
+print("Capital inicial = 10000")
+print("al 2% con interes compuesto")
+print("Despues de 36 meses(3 A%os):")
+print(total)
